f69415292c98469f45357e8a329b850c83dc7e6f
Israa Qaba <israa.qaba98@gmail.com>
2022-04-12 17:59:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"]
+console.log(cats)
+
+function destructivelyAppendCat(name){
+  cats.push('Ralph')
+}
+
+function destructivelyPrependCat(name){
+  cats.unshift('Bob')
+}   
+
+function destructivelyRemoveLastCat(){
+  cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift()
+}
+
+function appendCat(name){
+  const newArray=[...cats]
+  newArray.push(name);
+  return newArray;
+} 
+
+function prependCat(name){
+  return [name, ...cats]
+} 
+
+function removeLastCat(){
+  return cats.slice(0, cats.length -1)
+} 
+
+
+
+function removeFirstCat(){
+  return cats.slice(1, cats.length)
+}
+
+// The output works but it doesn't pass the test thats why I commented it
+/* function removeLastCat(cats){
+  cats.pop();
+} 
+removeLastCat(cats, 'Garfield')
+console.log(cats);
+*/
